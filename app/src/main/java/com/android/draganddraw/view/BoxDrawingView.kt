4557647a8f439e588f328d01bb5506384100953d
Michael_0619 <any_leon@163.com>
2023-07-06 08:51:06 +0800

    [BoxDrawingView]Build BoxDrawingView

---
+++ app/src/main/java/com/android/draganddraw/view/BoxDrawingView.kt
@@ -0,0 +1,9 @@
+package com.android.draganddraw.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class BoxDrawingView(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
+
+}
